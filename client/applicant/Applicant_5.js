4eb2495b21cb42f5fef22976a70f4584ab291902
jmlee <jmlee2109@naver.com>
2019-10-27 23:47:26 +0900

    .

--- client/applicant/Applicant_5.js
+++ client/applicant/Applicant_5.js
@@ -15,6 +15,18 @@ Template.Applicant_5.onRendered(function() {
     run()
   }, 1000)
 
+  Template.Applicant_5.helpers({
+    boards: function() {
+      return DB_RESUME.findAll({}, {sort: {createdAt: -1}});
+    },
+    YMD: function() {
+      return this.createdAt.toStringYMD();
+    },
+    HMS: function() {
+      return this.createdAt.toStringHMS();
+    }
+  });
+
   Meteor.call('savePost', infoData, function(err, infoData) {
     //서버에서 처리 할거 다하고 응답을 주면 그 리턴을 받아서 클라이언트가 실행하는 부분
     if(err) {
