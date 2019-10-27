754e83657de532c396cdcf5e93462576ab01288b
jmlee <jmlee2109@naver.com>
2019-10-28 02:02:05 +0900

    Merge branch 'master' into JiMin
    # Conflicts:
    #	client/applicant/Applicant_1.html
    #	client/applicant/Applicant_5.html
    #	client/applicant/Applicant_5.js
    #	server/main.js

--- client/applicant/Applicant_5.js
+++ client/applicant/Applicant_5.js
@@ -1,54 +1,13 @@
-//
-// FlowRouter.route('/Applicant_5', {
-//   action: function() {
-//     BlazeLayout.render('Applicant_5');
-//   }
-// })
-//
-// Template.Applicant_5.onRendered(function() {
-//   run = function () {
-//     feather.replace()
-//   };
-//
-//
-//   Meteor.setTimeout(function(){
-//     run()
-//   }, 1000)
-//
-//   Template.Applicant_5.helpers({
-//     boards: function() {
-//       return Meteor.users.find({}, {sort: {createdAt: -1}});
-//     },
-//     YMD: function() {
-//       return this.createdAt.toStringYMD();
-//     },
-//     HMS: function() {
-//       return this.createdAt.toStringHMS();
-//     }
-//   });
-//
-//   var phoneNumber = $('#phoneNumber').val();
-//   var homeAdd = $('#homeAdd').val();
-//   var email = $('#email').val();
-//   var gender = $('#gender').val();
-//   var birth = $('#birth').val();
-//
-//
-//   var infoData = {
-//     phoneNumber: phoneNumber,
-//     homeAdd: homeAdd,
-//     email: email,
-//     gender: gender,
-//     birth: birth
-//   }
-//
-//   Meteor.call('savePost', infoData, function(err, infoData) {
-//     //서버에서 처리 할거 다하고 응답을 주면 그 리턴을 받아서 클라이언트가 실행하는 부분
-//     if(err) {
-//       alert(err);
-//     } else {
-//       find(infoData); //결과를 가지고 클라이언트에서 수행해야 하는 것이 있으면 여기서 하면 됨.
-//     }
-//   });
-//
-// });
+FlowRouter.route('/Applicant_5', {
+    action: function() {
+        BlazeLayout.render('Applicant_5');
+    }
+})
+Template.Applicant_5.onRendered(function() {
+    run = function () {
+        feather.replace()
+    };
+    Meteor.setTimeout(function(){
+        run()
+    }, 1000)
+});
