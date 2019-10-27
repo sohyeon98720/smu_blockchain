70507815e428bf5cd85712c271b1a4237f3c76ca
jmlee <jmlee2109@naver.com>
2019-10-27 22:35:40 +0900

    .

--- client/applicant/Applicant_5.js
+++ client/applicant/Applicant_5.js
@@ -7,11 +7,14 @@ FlowRouter.route('/Applicant_5', {
 Template.Applicant_5.onRendered(function() {
   run = function () {
     feather.replace()
-
   };
 
 
   Meteor.setTimeout(function(){
     run()
   }, 1000)
+
+
+
+
 });
