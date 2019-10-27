442af5074ff42ec2bdc942527be394dd340432f4
gigiolo <piggutail79@gmail.com>
2019-10-28 04:48:09 +0900

    테이블로 디비 정보 보임

--- client/university/Uni.js
+++ client/university/Uni.js
@@ -15,6 +15,12 @@ Template.Uni.onRendered(function() {
 
 Template.Uni.helpers({
   students: function() {
+    // return Meteor.users.find({UniApply: 'true', }).fetch();
+    // return Meteor.users.find({p_Type: '상명대학교'}).fetch();
     return Meteor.users.find({_id:Meteor.user()._id}).fetch();
+  },
+  students2: function() {
+    return Meteor.users.find({UniApply: 'true'}, {"p_Name": true, "birth": true, "email": true, "uni_Name": true, "uni_Author":true})[18].fetch();
   }
+
 })
