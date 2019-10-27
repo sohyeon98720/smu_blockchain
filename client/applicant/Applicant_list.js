98bd56a023913c7f528cfcab4a9080b064ecbe04
gigiolo <piggutail79@gmail.com>
2019-10-28 01:49:27 +0900

    내원서함 목록 페이지를 왼쪽에 추가함

--- client/applicant/Applicant_list.js
+++ client/applicant/Applicant_list.js
@@ -1,4 +1,4 @@
-FlowRouter.route('/Applicant_list/:_id', {
+FlowRouter.route('/Applicant_list', {
     action: function() {
         BlazeLayout.render('Applicant_list');
     }
@@ -16,8 +16,7 @@ Template.Applicant_list.onRendered(function() {
 });
 
 Template.Applicant_list.helpers({
-    userInfo: function() {
-        var _id=FlowRouter.getParam('_id')
-        return users.findOne({_id: _id});
+    userInfoList: function() {
+        return users.find({});
     }
 })
