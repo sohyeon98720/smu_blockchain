3c9f873702eb0a95a0266a4a0d44033006d8db51
jmlee <jmlee2109@naver.com>
2019-10-29 18:06:24 +0900

    Merge branch 'master' into JiMin
    # Conflicts:
    #	client/applicant/Applicant_1.html
    #	client/applicant/Applicant_5.html
    #	client/applicant/Applicant_5.js
    #	server/main.js

--- client/applicant/Applicant_5.js
+++ client/applicant/Applicant_5.js
@@ -18,6 +18,11 @@ Template.Applicant_5.onRendered(function() {
 Template.Applicant_5.helpers({
   userInfo: function() {
     return Meteor.users.findOne({_id:Meteor.user()._id});
+  },
+
+  link : function() {
+    // 컨테츠 데이터베이스에 저장 되어 있는 파일(이미지)의 _id를 이용하여 실제 링크로 변환하여 전달
+    return Codeasy.utils.getFileLink(this.file._id)
   }
 })
 
