42fcec00852fd333efebe7d9df80ad689b5ae488
nahye <nahelove03@gmail.com>
2019-11-01 01:17:43 +0900

    기업 페이지 권한

--- client/university/Uni.js
+++ client/university/Uni.js
@@ -1,20 +1,20 @@
-FlowRouter.route('/Uni', {
-  action: function() {
-    BlazeLayout.render('Uni');
-  }
-})
-
-
 // FlowRouter.route('/Uni', {
 //   action: function() {
-//     var userInfo = Meteor.user();
-//     if(userInfo.profile.p_Type === "학교"){
-//       BlazeLayout.render('Uni');
-//     }else{
-//       alert("권한이 없습니다.")
-//     }
+//     BlazeLayout.render('Uni');
 //   }
 // })
+
+
+FlowRouter.route('/Uni', {
+  action: function() {
+    var userInfo = Meteor.user();
+    if(userInfo.profile.p_Type === "학교"){
+      BlazeLayout.render('Uni');
+    }else{
+      alert("권한이 없습니다.")
+    }
+  }
+})
 
 Template.Uni.helpers({
   students: function() {
